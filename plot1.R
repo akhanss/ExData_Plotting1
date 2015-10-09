dfc2db464392163e15da9752949b7a8764e5721e
akhanss <a_khanss@yahoo.com>
2015-10-09 12:40:01 +0600

    Second Commit
    Minor modification

--- plot1.R
+++ plot1.R
@@ -7,10 +7,10 @@ download.file(fileUrl, destFile, method = "curl")
 unzip("./household_power_consumption.zip")
 
 ## Read the data from just those dates of "1/2/2007" and "2/2/2007"
-dt.sub <- subset(read.table("./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors=FALSE), Date == "1/2/2007" | Date == "2/2/2007")
+dt.sub <- subset(read.table("./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE), Date == "1/2/2007" | Date == "2/2/2007")
 
 ## Define PNG file with a width of 480 pixels and a height of 480 pixels
-png("plot1.png", width=480, height=480)
+png("plot1.png", width = 480, height = 480)
 
 ## Plotting graph
 hist(dt.sub$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
